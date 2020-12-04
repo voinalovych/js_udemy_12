c2bf4170cad6d701bae234d9ddb189da756f5fe2
Voinalovych_Oleksandr <sancho.voin@gmail.com>
2020-12-04 22:02:13 +0200

    js_12 lesson,first exersise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB =  {
+    count: numberOfFilms,
+    movies: {},
+    actor: {},
+    genres: [],
+    privat: false,
+};
+
+const watchedMovie = prompt('Один из последних просмотренных фильмов', ''),
+      moviePoints = +prompt('На сколько оцените его?', ''),
+      watchedMovie2 = prompt('Один из последних просмотренных фильмов', ''),
+      moviePoints2 = +prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[watchedMovie] = moviePoints;
+personalMovieDB.movies[watchedMovie2] = moviePoints2;
+
+console.log(personalMovieDB);
